0b182f2b80a24a5fe89565638c7e6186f0d70666
NB <nader.baydoun@dawsoncollege.qc.ca>
2020-05-28 14:02:14 -0400

    Tic-Tac-Toe not finding the optimal value

--- solution_tictactoe.py
+++ solution_tictactoe.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-
+import math
 #####
 # VotreNom (VotreMatricule) .~= À MODIFIER =~.
 ###
@@ -8,6 +8,7 @@ from pdb import set_trace as dbg  # Utiliser dbg() pour faire un break dans votr
 
 import random
 import numpy as np
+
 
 ########################
 # Solution tic-tac-toe #
@@ -30,13 +31,50 @@ import numpy as np
 #
 # retour: Cette fonction retourne l'action optimal à joueur pour le joueur actuel c.-à-d. 'str_joueur'.
 ###
+def joueur_tictactoe(etat, fct_but, fct_transitions, str_joueur):
+    # TODO: Implémenter un joueur alpha-beta
+    # Find the next best move
+    # This function needs to find the most efficient NEXT STEP to the game depending on the player
+    # Do we really need to go into depth or we only need to check the following function?
+    value = (alphabeta(-math.inf, math.inf, etat, fct_but, fct_transitions, str_joueur))
+    #value = []
+    #print(str_joueur)
+    #for i in fct_transitions(etat).items():
+        #value.append(alphabeta(-math.inf, math.inf, i[1], fct_but, fct_transitions, str_joueur))
+    print(value)
+    print("Done")
+    # childrenStates = fct_transitions(etat).items()
+    # for x in childrenStates:
+    #    print(fct_but(x[1]))
 
 
-def joueur_tictactoe(etat,fct_but,fct_transitions,str_joueur):
-
-    #TODO: Implémenter un joueur alpha-beta
 
     # Retourne une action aléatoire (.~= À MODIFIER =~.)
-
     action = random.choice(list(fct_transitions(etat)))
     return action
+
+
+def alphabeta(alpha, beta, etat, fct_but, fct_transitions, str_joueur):
+    if fct_but(etat) is not None:
+        return fct_but(etat)
+
+    states = fct_transitions(etat).items()
+
+    if str_joueur == 'X':
+        maxEval = -math.inf
+        for children in states:
+            eval = alphabeta(alpha, beta, children[1], fct_but, fct_transitions, 'O')
+            maxEval = max(maxEval, eval)
+            alpha = max(alpha, eval)
+            if beta <= alpha:
+                break
+        return maxEval
+    else:
+        minEval = math.inf
+        for children in states:
+            eval = alphabeta(alpha, beta, children[1], fct_but, fct_transitions, 'X')
+            minEval = min(minEval, eval)
+            beta = min(beta, eval)
+            if beta <= alpha:
+                break
+        return minEval
